ab2fd3658c4a7d7026cf5efafa27a320e2f0989d
Balázs Réka <balazsreka@ktch.hu>
2023-03-02 19:23:24 +0100

    Készen van

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
+
+const tbody = document.querySelector("#tbody");
+const saveButton = document.querySelector("#saveButton");
+
+const nameInput = document.querySelector("#name");
+const quantityInput = document.querySelector("#quantity");
+const priceInput = document.querySelector("#price");
+
+const editidInput = document.querySelector('#editid');
+const editnameInput = document.querySelector('#editname');
+const editquantityInput = document.querySelector('#editquantity');
+const editpriceInput = document.querySelector('#editprice');
+
+const saveEditButton = document.querySelector('#saveEditButton');
+
 const gyumolcsok = [
     {id: 1, name:'szilva', quantity:35, price:8},
     {id: 2, name:'alma', quantity:45, price:8.3},
     {id: 3, name:'körte', quantity:25, price:9.5},
     {id: 4, name:'barack', quantity:37, price:12}
 ];
-const tbody = document.querySelector("#tbody");
+
 
 function generateTbody(){
     gyumolcsok.forEach((gyumolcs) => {
@@ -25,6 +40,7 @@ function generateTbody(){
         tr.append(tdQuantity);
         tr.append(tdPrice);
         tr.append(generateTdDelete(gyumolcs.id));
+        tr.append(generateTdEdit(gyumolcs));
     });
 }
 generateTbody();
@@ -53,3 +69,72 @@ function generateTdDelete(id) {
     td.append(button);
     return td;
 }
+
+
+
+function generateTdEdit(fruit) {
+    let td = document.createElement('td');
+    let button = document.createElement('button');
+    button.textContent = "Szerkesztés";
+    button.classList = "btn btn-info";
+
+    button.setAttribute('data-bs-toggle', 'modal');
+    button.setAttribute('data-bs-target', '#editModal');
+
+    button.addEventListener('click',() =>{
+        console.log('működik');
+        console.log(fruit.name);
+
+         editidInput.value = fruit.id;
+         editnameInput.value = fruit.name;
+         editquantityInput.value = fruit.quantity;
+         editpriceInput.value = fruit.price;
+
+    });
+    td.append(button);
+    return td;
+}
+
+saveButton.addEventListener('click', () => {
+    
+    let name = (nameInput.value);
+    let quantity = (quantityInput.value);
+    let price = (priceInput.value);
+    let gyumolcs = {
+        name: name, 
+        quantity: quantity, 
+        price:price
+    };
+    gyumolcsok.push(gyumolcs);
+    console.log(gyumolcsok);
+    tbody.textContent = '';
+    generateTbody();
+    clearFieldOnAddModal();
+});
+
+function clearFieldOnAddModal() {
+    nameInput.value = '';
+    quantityInput.value = '';
+    priceInput.value = '';
+}
+
+saveEditButton.addEventListener('click', () => {
+
+    let id = editidInput.value;
+    let name = editnameInput.value;
+    let quantity = editquantityInput.value;
+    let price = editpriceInput.value;
+
+    gyumolcsok.forEach(  (gyumolcs) => {
+        
+        if (gyumolcs.id == id){
+            gyumolcs.name = name;
+            gyumolcs.quantity = quantity;
+            gyumolcs.price = price;
+        }
+    });
+
+    tbody.textContent ='';
+    generateTbody();
+
+})
